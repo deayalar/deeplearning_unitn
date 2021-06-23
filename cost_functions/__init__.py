e7afb38bdb60a760eb8d7b727c65bf2e2c85228b
David Ayala <deayalar@gmail.com>
2021-06-23 16:09:17 +0200

    Remove code we are not using

--- cost_functions/__init__.py
+++ cost_functions/__init__.py
@@ -1,3 +1,4 @@
+import numpy as np
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -25,46 +26,9 @@ class AttributesLossWrapper(nn.Module):
 
         loss_age = crossEntropy(preds[0], attrs[:,0])
 
-        #loss_backpack = bce(preds[1], attrs[:,1].unsqueeze(1).to(torch.float32))
-        #loss_bag = bce(preds[2], attrs[:,2].unsqueeze(1).to(torch.float32))
-        #loss_handbag = bce(preds[3], attrs[:,3].unsqueeze(1).to(torch.float32))
-        #loss_clothes = bce(preds[4], attrs[:,4].unsqueeze(1).to(torch.float32))
-        #loss_down = bce(preds[5], attrs[:,5].unsqueeze(1).to(torch.float32))
-        #loss_up = bce(preds[6], attrs[:,6].unsqueeze(1).to(torch.float32))
-        #loss_hair = bce(preds[7], attrs[:,7].unsqueeze(1).to(torch.float32))
-        #loss_hat = bce(preds[8], attrs[:,8].unsqueeze(1).to(torch.float32))
-        #loss_gender = bce(preds[9], attrs[:,9].unsqueeze(1).to(torch.float32))
-        #loss_upblack = bce(preds[10], attrs[:,10].unsqueeze(1).to(torch.float32))
-        #loss_upwhite = bce(preds[11], attrs[:,11].unsqueeze(1).to(torch.float32))
-        #loss_upred = bce(preds[12], attrs[:,12].unsqueeze(1).to(torch.float32))
-        #loss_uppurple = bce(preds[13], attrs[:,13].unsqueeze(1).to(torch.float32))
-        #loss_upyellow = bce(preds[14], attrs[:,14].unsqueeze(1).to(torch.float32))
-        #loss_upgray = bce(preds[15], attrs[:,15].unsqueeze(1).to(torch.float32))
-        #loss_upblue = bce(preds[16], attrs[:,16].unsqueeze(1).to(torch.float32))
-        #loss_upgreen = bce(preds[17], attrs[:,17].unsqueeze(1).to(torch.float32))
-        #loss_downblack = bce(preds[18], attrs[:,18].unsqueeze(1).to(torch.float32))
-        #loss_downwhite = bce(preds[19], attrs[:,19].unsqueeze(1).to(torch.float32))
-        #loss_downpink = bce(preds[20], attrs[:,20].unsqueeze(1).to(torch.float32))
-        #loss_downpurple = bce(preds[21], attrs[:,21].unsqueeze(1).to(torch.float32))
-        #loss_downyellow = bce(preds[22], attrs[:,22].unsqueeze(1).to(torch.float32))
-        #loss_downgray = bce(preds[23], attrs[:,23].unsqueeze(1).to(torch.float32))
-        #loss_downblue = bce(preds[24], attrs[:,24].unsqueeze(1).to(torch.float32))
-        #loss_downgreen = bce(preds[25], attrs[:,25].unsqueeze(1).to(torch.float32))
-        #loss_downbrown = bce(preds[26], attrs[:,26].unsqueeze(1).to(torch.float32))
-
         binary_losses = 0
         for idx in range(1, len(preds)):
             binary_losses += bce(preds[idx], attrs[:, idx].unsqueeze(1).to(torch.float32))
-
-        #precision0 = torch.exp(-self.log_vars[0])
-        #loss0 = precision0*loss0 + self.log_vars[0]
-
-        # precision1 = torch.exp(-self.log_vars[1])
-        # loss1 = precision1*loss1 + self.log_vars[1]
-
-        # precision2 = torch.exp(-self.log_vars[2])
-        # loss2 = precision2*loss2 + self.log_vars[2]
-        
         return loss_age + binary_losses
 
 class TripletLoss(nn.Module):
@@ -87,6 +51,9 @@ class TripletLoss(nn.Module):
             inputs: feature matrix with shape (batch_size, feat_dim)
             targets: ground truth labels with shape (num_classes)
         """
+        #print(targets)
+        #print(inputs.size())
+        targets = torch.Tensor(np.array([int(el) for el in targets]))
         n = inputs.size(0)
         # inputs = 1. * inputs / (torch.norm(inputs, 2, dim=-1, keepdim=True).expand_as(inputs) + 1e-12)
         # Compute pairwise distance, replace by the official when merged
@@ -104,6 +71,9 @@ class TripletLoss(nn.Module):
         dist_an = torch.cat(dist_an)
         # Compute ranking hinge loss
         y = torch.ones_like(dist_an)
+        #print("dist_an", dist_an)
+        #print("dist_ap", dist_ap)
+        #print(y)
         loss = self.ranking_loss(dist_an, dist_ap, y)
         if self.mutual:
             return loss, dist
